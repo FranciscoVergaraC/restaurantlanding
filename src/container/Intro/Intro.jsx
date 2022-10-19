bb20ed0946ca1889d91c354562e8ecf40d9aa37e
FranciscoVergaraC <franciscovergaracr@gmail.com>
2022-10-19 09:06:25 -0300

    Chef section ready

--- src/container/Intro/Intro.jsx
+++ src/container/Intro/Intro.jsx
@@ -1,11 +1,24 @@
 import React from 'react';
+import {BsFillPlayFill, BsPauseFill} from 'react-icons/bs';
+import {meal} from '../../constants'
 
 import './Intro.css';
 
-const Intro = () => (
-  <div>
+const Intro = () => {
+  const vidRef = React.useRef();
+  
+  return (
+  <div className='app__vide'>
+    <video
+      src={meal}
+      ref={vidRef}
+      type='video/mp4'
+      loop
+      controls={false}
+      muted
+    />
     Intro
   </div>
-);
+)};
 
 export default Intro;
